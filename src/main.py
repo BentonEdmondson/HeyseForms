fd5f9829b1f0a4c63f2555b70d202508b81192fc
Chin <jeonghin@UMICH.EDU>
2022-06-29 00:21:50 -0400

    Fixed breadcrumb

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 from http.client import responses
 from flask import Flask
-from endpoints import home, responses, settings, api
+from endpoints import home, responses, settings, api, settingsadmin
 
 app = Flask(__name__, template_folder="./templates")
 
 app.register_blueprint(home.home)
 app.register_blueprint(responses.responses)
 app.register_blueprint(settings.settings)
+app.register_blueprint(settingsadmin.settingsadmin)
 app.register_blueprint(api.api)
 
 if __name__ == "__main__":
