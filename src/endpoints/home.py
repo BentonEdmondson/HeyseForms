07655b99980187e04f653a4dfee3867b8700449e
Benton Edmondson <bentonedmondson@gmail.com>
2022-06-29 12:41:29 -0400

    updated homepage to match figma

--- src/endpoints/home.py
+++ src/endpoints/home.py
@@ -9,5 +9,19 @@ home.route('/', methods=["GET"])(
 
 @home.route('/home', methods=['GET'])
 def get_home():
-    interns = gsheets.get_supervisor_interns(supervisor_email="jjc@umich.edu")
+    #interns = gsheets.get_supervisor_interns(supervisor_email="jjc@umich.edu")
+    interns = [
+        {
+            "uniqname": "benton",
+            "progress": 0.43
+        },
+        {
+            "uniqname": "amadeus",
+            "progress": 0.12
+        },
+        {
+            "uniqname": "lordvader",
+            "progress": 1
+        }
+    ]
     return render_template('home.j2', interns=interns)
