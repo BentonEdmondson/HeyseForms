d307178ff284247f569b5818a44a08821937883b
Atharva Kulkarni <atharvak@umich.edu>
2022-06-30 17:19:05 -0400

    Toggle Switch For Reminder Conncected With Spreadsheet

--- src/sheets_api/sheets.py
+++ src/sheets_api/sheets.py
@@ -21,12 +21,13 @@ EMAIL_COLUMN_NAME = "Email Address"
 def check_credentials(func):
     def wrapper(*args, **kwargs):
         creds = None
-        # The file token.json stores the user's access and refresh tokens, and is
-        # created automatically when the authorization flow completes for the first
-        # time.
         if os.path.exists(SERVICE_ACCOUNT_FILE):
             creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-            return func(creds=creds, *args, **kwargs)
+            try: 
+                return func(creds=creds, *args, **kwargs)
+            except HttpError as err:
+                print(err)
+                return
         
         print("Could not find service account credentials.")
 
@@ -38,29 +39,24 @@ def get_all_intern_entries(creds: Credentials) -> list:
     """
     Gets all the entries of an intern.
     """
-    try:
-        result = []
+    result = []
 
-        service = build('sheets', 'v4', credentials=creds)
-        sheet = service.spreadsheets()
-        my_range = "Response!A1:E"
+    service = build('sheets', 'v4', credentials=creds)
+    sheet = service.spreadsheets()
+    my_range = "Response!A1:E"
 
-        data = sheet.values().get(spreadsheetId=HEYSE_FORMS_SAMPLE_SPREADSHEET_ID,
-                                    range=my_range).execute()
-        values = data.get('values', [])
+    data = sheet.values().get(spreadsheetId=HEYSE_FORMS_SAMPLE_SPREADSHEET_ID,
+                                range=my_range).execute()
+    values = data.get('values', [])
 
-        result = []
+    result = []
 
-        if len(values) > 1:
-            for row in values[1:]:
-                entry = convert_to_dict(values[0], row)
-                result.append(entry)
-        
-        return result
-
-    except HttpError as err:
-        print(err)
-        return
+    if len(values) > 1:
+        for row in values[1:]:
+            entry = convert_to_dict(values[0], row)
+            result.append(entry)
+    
+    return result
 
 
 def get_intern_entries(intern_email: str) -> dict:
@@ -74,27 +70,22 @@ def get_intern_entries(intern_email: str) -> dict:
 
 @check_credentials
 def get_all_supervisor_data(creds: Credentials) -> list:
-    try:
-        service = build('sheets', 'v4', credentials=creds)
-        sheet = service.spreadsheets()
+    service = build('sheets', 'v4', credentials=creds)
+    sheet = service.spreadsheets()
 
-        my_range = "Record!A1:D"
-        data = sheet.values().get(spreadsheetId=HEYSE_FORMS_SAMPLE_SPREADSHEET_ID,
-                                    range=my_range, majorDimension="COLUMNS").execute()
-        values = data.get('values', [])
+    my_range = "Record!A1:D"
+    data = sheet.values().get(spreadsheetId=HEYSE_FORMS_SAMPLE_SPREADSHEET_ID,
+                                range=my_range, majorDimension="COLUMNS").execute()
+    values = data.get('values', [])
 
-        result = []
+    result = []
 
-        if len(values) > 1:
-            for row in values[1:]:
-                entry = convert_to_dict(values[0], row)
-                result.append(entry)
-        
-        return result
-
-    except HttpError as err:
-        print(err)
-        return
+    if len(values) > 1:
+        for row in values[1:]:
+            entry = convert_to_dict(values[0], row)
+            result.append(entry)
+    
+    return result
 
 
 def get_supervisor_interns(supervisor_email: str) -> list:
@@ -109,17 +100,6 @@ def get_supervisor_interns(supervisor_email: str) -> list:
                 })
     return result
 
-def get_all_interns() -> list:
-    supervisors = get_all_supervisor_data()
-    result = []
-    for supervisor in supervisors:
-        interns = supervisor["Interns"].split(", ")
-        for intern in interns:
-            result.append({
-                "uniqname": intern
-            })
-    return result
-
 
 def get_supervisor_notifcation(supervisor_email: str) -> bool:
     supervisors = get_all_supervisor_data()
@@ -130,6 +110,24 @@ def get_supervisor_notifcation(supervisor_email: str) -> bool:
             return False
 
     return None
+
+
+@check_credentials
+def update_supervisor(supervisor_data: OrderedDict, my_range: str, creds: Credentials):
+            
+    service = build('sheets', 'v4', credentials=creds)
+
+    body = {
+        "majorDimension": "COLUMNS",
+        "values": [list(supervisor_data.values())]
+    }
+
+    response = service.spreadsheets().values().update(
+            spreadsheetId=HEYSE_FORMS_SAMPLE_SPREADSHEET_ID, range=my_range,
+            valueInputOption="USER_ENTERED", body=body).execute()
+
+    print(response)
+
 
 def convert_to_dict(keys: list, values: list) -> dict:
     return OrderedDict(zip(keys, values))
