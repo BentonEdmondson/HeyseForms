b859f09391bfbf8a94b0f456ceb44761579b78fb
porygonisaduck <alvaradx@umich.edu>
2022-07-05 13:06:45 -0400

    Moved to gunicorn and restructured

--- readme.md
+++ readme.md
@@ -4,7 +4,8 @@
 
 ```
 pip install -r requirements.txt
-python src/main.py
+cd src
+python3 main.py
 ```
 
 ## Technical
---
+++ src/guniconf.py
@@ -0,0 +1,12 @@
+# for deployment
+bind = '0.0.0.0:8000'
+workers = 3
+
+# log to stdout - handy for docker
+accesslog = '-'
+
+# log to stderr - handy to docker
+errorlog = '-'
+
+# provide X-Forwarded-For header for openshift deployment
+access_log_format = '%({X-Forwarded-For}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s %(L)s'
