852d700f9bd18a92a0e4cee51e7a66eba4377cec
porygonisaduck <alvaradx@umich.edu>
2022-07-09 13:34:37 -0400

    Fixed serivce.json error

--- src/sheets_api/sheets.py
+++ src/sheets_api/sheets.py
@@ -11,7 +11,7 @@ from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SERVICE_ACCOUNT_FILE = 'src/service.json'
+SERVICE_ACCOUNT_FILE = 'service.json'
 
 # The ID of HeyseForms sample spreadsheet.
 HEYSE_FORMS_SAMPLE_SPREADSHEET_ID = '1ymUE8AJEEyXvfLML8PNVbs-sI3poYKq1Vw2_HKTR4qw'
