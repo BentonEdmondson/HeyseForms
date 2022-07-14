5a2005d2633231828b6efdab1878d2387549fd3f
jeonghin <jeonghin@gmail.com>
2022-07-14 00:41:41 -0400

    Fixed Google Spreadsheet link

--- src/endpoints/gsheet.py
+++ src/endpoints/gsheet.py
@@ -4,11 +4,9 @@ import sheets_api.sheets as gsheets
 
 gsheet = Blueprint('gsheet', __name__)
 
-gsheet.route('/', methods=["GET"])(
-    lambda: redirect('/gsheet', code=308)
-)
 
 @gsheet.route('/gsheet', methods=['GET'])
 def get_gsheet():
     link = gsheets.get_spreadsheet_URL()
-    return render_template('gsheet.j2', link = link)
+    return redirect(link, code=302)
+    #return render_template('gsheet.j2', link = link)
