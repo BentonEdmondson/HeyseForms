ba10374916c21b53964b2426bda899d15f6292a5
porygonisaduck <alvaradx@umich.edu>
2022-07-26 14:54:47 -0400

    Fixing escaping \ issue

--- src/sheets_api.py
+++ src/sheets_api.py
@@ -13,7 +13,9 @@ from decouple import config
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-PRIVATE_KEY = f"-----BEGIN PRIVATE KEY-----\n{config('private_key')}\n-----END PRIVATE KEY-----\n"
+PRIVATE_KEY = f"-----BEGIN PRIVATE KEY-----\n{config('private_key')} \n-----END PRIVATE KEY-----\n"
+PRIVATE_KEY = PRIVATE_KEY.replace(r'\\','\\')) 
+
 SERVICE_ACCOUNT_INFO = {"type": config('type'),
                         "project_id": config('project_id'),
                         "private_key_id": config('private_key_id'),
